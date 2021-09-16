6ab3869a9f47daa68fe6fe2718f5276086c2920e
lifengcun <lifengcun@meituan.com>
2021-09-16 15:13:58 +0800

    unlinkLink-2

--- install.old_balls.sh
+++ install.old_balls.sh
@@ -9,28 +9,29 @@ unlinkLink() {
     fi
     ln -s $1 $2
     echo "Linked $2 to $1"
+    echo "---------------"
 }
 # vim
-unlinkLink -s ${BASEDIR}/vim/vimrc ~/.vimrc
-unlinkLink -s ${BASEDIR}/vim/vim/ ~/.vim
+unlinkLink ${BASEDIR}/vim/vimrc ~/.vimrc
+unlinkLink ${BASEDIR}/vim/vim/ ~/.vim
 
 # zsh
-unlinkLink -s ${BASEDIR}/zsh/zshrc ~/.zshrc
+unlinkLink ${BASEDIR}/zsh/zshrc ~/.zshrc
 
 #bash
-unlinkLink -s ${BASEDIR}/bash/inputrc ~/.inputrc
+unlinkLink ${BASEDIR}/bash/inputrc ~/.inputrc
 if [[ -L ~/.bashrc ]]; then
   unlink ~/.bashrc
 fi
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc_local_backup
-unlinkLink -s ${BASEDIR}/bash/bashrc ~/.bashrc
+unlinkLink ${BASEDIR}/bash/bashrc ~/.bashrc
 
 # git
-# unlinkLink -s ${BASEDIR}/gitconfig ~/.gitconfig
+# unlinkLink ${BASEDIR}/gitconfig ~/.gitconfig
 
 # tmux
 
-unlinkLink -s ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
+unlinkLink ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
 
 # FZF
-unlinkLink -s ${BASEDIR}/fzf/ ~/.fzf
+unlinkLink ${BASEDIR}/fzf/ ~/.fzf
